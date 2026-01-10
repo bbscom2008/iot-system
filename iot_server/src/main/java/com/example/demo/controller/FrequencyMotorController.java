c5be782395ec1038acd94792352bda2769ca11a7
bbscom2008 <bbscom2008@gmail.com>
2026-01-10 16:04:15 +0800

    11

--- iot_server/src/main/java/com/example/demo/controller/FrequencyMotorController.java
+++ iot_server/src/main/java/com/example/demo/controller/FrequencyMotorController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/frequency-motor")
+@RequestMapping("/frequencyMotor")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class FrequencyMotorController {
